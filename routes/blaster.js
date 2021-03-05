c0bdff05cef7ee6167f8debfe9d62534ce7828f4
tech@strangersoccer.com <a.pranasakti@gmail.com>
2021-03-05 23:11:41 +0700

    update blaster.js

--- routes/blaster.js
+++ routes/blaster.js
@@ -120,7 +120,7 @@ router.post('/generateReminders', async (req,res) => {
 
 		}
 			
-		res.send('Email blast successfully sent.');
+		res.send({message: 'Email blast successfully sent.'});
 	} catch(err){
 		res.status(500).send({error: true, message: err.toString()});
 	}
