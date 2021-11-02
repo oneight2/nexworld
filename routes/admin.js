f9902b17046e95a5e5f5a5e8198a30c7d7450410
oneight2 <syarif.cibatu@gmail.com>
2021-11-02 08:29:23 +0700

    api

--- routes/admin.js
+++ routes/admin.js
@@ -1,99 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
-const pgdb = require('../db/pg');
-
-const bcrypt = require('bcrypt');
-
-//MIDDLEWARE//
-const authMw = require('../middleware/authToken')
-
-function parsemessage(code){
-  switch(code){
-    case '1':
-    return 'Delete successful!'
-    break;
-    case '2':
-    return 'Add successful!'
-    break;
-  }
-}
-
-router.get('/', function (req, res) {
-  res.send('Get Admin');
-})
-
-router.post('/', authMw.authToken({permissions: ['admin']}), async (req,res)=>{
-  try{
-    res.send({status: 'Success', message: 'Authorized'});
-  } catch(err){
-    res.send({status: 'Error', message: 'Unauthorized'})
-  }
-})
-
-router.get('/dashboard', async (req, res) => {
-  try {
-    res.render('admin_dashboard', {
-      title: 'Synnex Admin', 
-      layout: 'layouts/adminsidenav'
-    });
-  } catch(err){
-    res.send(err.toString());
-  }  
-})
-
-router.get('/booths', async (req, res) => {
-  try {
-    let booths = await pgdb.getBooths();
-
-    res.render('admin_booths', {
-      title: 'Synnex Admin', 
-      layout: 'layouts/adminsidenav',
-      booths: booths,
-      message: req.query.message ? parsemessage(req.query.message) : null
-    });
-  } catch(err){
-    res.send(err.toString());
-  }  
-})
-
-router.get('/addbooths', async (req, res) => {
-  try {
-    let booths = await pgdb.getBooths();
-
-    res.render('admin_addbooth', {
-      title: 'Synnex Admin', 
-      layout: 'layouts/adminsidenav',
-      message: req.query.message ? parsemessage(req.query.message) : null
-    });
-  } catch(err){
-    res.send(err.toString());
-  }  
-})
-
-router.get('/annotations_selectbooth', async (req, res) => {
-  try{
-    let booths = await pgdb.getBooths();
-
-    res.render('admin_selectbooth', {
-      title: 'Synnex Admin - Annotations', 
-      layout: 'layouts/adminsidenav', 
-      extractScripts: true,
-      booths: booths
-    });
-  } catch(err){
-    res.send(err.toString());
-  }
-})
-
-router.post('/annotations_selectbooth', (req, res) => {
-  res.redirect('/admin/annotations?boothid='+req.body.boothid);
-})
-
-router.get('/annotations', async (req, res) => {
-  try{
-=======
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
@@ -192,73 +96,20 @@ router.post("/annotations_selectbooth", (req, res) => {
 
 router.get("/annotations", async (req, res) => {
   try {
->>>>>>> 6a80a6f8e4022272414fc0b24eff259f5b315ec2
     let annotations = await pgdb.getAnnotationsByBooth(req.query.boothid);
 
     let booth = await pgdb.getBooth(req.query.boothid);
     let boothId = booth[0].uid;
     let boothName = booth[0].name;
 
-<<<<<<< HEAD
-    res.render('admin_annotations', {
-      title: 'Synnex Admin - Annotations', 
-      layout: 'layouts/adminsidenav', 
-=======
     res.render("admin_annotations", {
       title: "Synnex Admin - Annotations",
       layout: "layouts/adminsidenav",
->>>>>>> 6a80a6f8e4022272414fc0b24eff259f5b315ec2
       extractScripts: true,
       annotations: annotations,
       boothId: boothId,
       boothName: boothName,
-<<<<<<< HEAD
-      message: req.query.message ? parsemessage(req.query.message) : null
-    });
-  } catch(err){
-    res.send(err.toString());
-  }
-})
 
-router.get('/addannotations', async (req, res) => {
-  try{
-    let booths = await pgdb.getBooths();
-
-    res.render('admin_addannotations', {
-      title: 'Synnex Admin - Annotations', 
-      layout: 'layouts/adminsidenav', 
-      extractScripts: true,
-      boothData: booths
-    });
-  } catch(err){
-    res.send(err.toString());
-  }
-})
-
-router.post('/getData', async (req, res) => {
-  try {
-  		let adminResponse = await pgdb.getAdmin();
-
-  		let adminUser = adminResponse[0].email;
-  		let adminPassword = adminResponse[0].password;
-
-  		let userMatch = req.body.user == adminUser;
-  		let passwordMatch = await bcrypt.compare(req.body.password, adminPassword);
-  		
-  		if(passwordMatch && userMatch){
-  			let response = await pgdb.getUsersData();
-
-	  		res.send(response);
-  		} else {
-  			res.status(500).send({error: true, message: 'Wrong username or password'})
-  		}
-	} catch (err){
-		res.status(500).send({error: true, message: err.toString()});
-	}
-})
-
-module.exports = router;
-=======
       message: req.query.message ? parsemessage(req.query.message) : null,
     });
   } catch (err) {
@@ -368,4 +219,3 @@ router.get("/bannerlist", async (req, res) => {
 });
 
 module.exports = router;
->>>>>>> 6a80a6f8e4022272414fc0b24eff259f5b315ec2
